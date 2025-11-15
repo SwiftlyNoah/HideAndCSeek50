cbdd346e0a71be8c0e59b6aa2faacb9b0802547f
SwiftlyNoah <devnoahapps@gmail.com>
2025-11-15 00:01:26 -0500

    Initial Commit

---
+++ HideAndCSeek50/HideAndCSeek50App.swift
@@ -0,0 +1,17 @@
+//
+//  HideAndCSeek50App.swift
+//  HideAndCSeek50
+//
+//  Created by Noah Brauner on 11/15/25.
+//
+
+import SwiftUI
+
+@main
+struct HideAndCSeek50App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
